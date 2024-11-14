f840e80af28253723b37426e1ab7fa5826fd47b3
Torvex <torvex.gen@gmail.com>
2024-11-14 06:09:10 +0200

    Initial commit

---
+++ linux/fish/config.fish
@@ -0,0 +1,65 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+
+
+# ==========  ALIASES ==========
+
+# Пакетний менеджер
+alias update='sudo apt update && sudo apt upgrade'  # Оновлення системи
+alias install='sudo apt install'                   # Встановити пакет
+alias remove='sudo apt remove'                     # Видалити пакет
+alias purge='sudo apt purge'                       # Видалити пакет та конфігураційні дані (Не видаляє налаштування з домашнього каталога)
+alias autoremove='sudo apt autoremove'             # Видаляє залежності, які не використовуються
+
+# Вивід інформації
+alias ll='ls -la'
+alias df='df -h'      # Ключ "-h" активує "людино-зрозумілий" (human-readable) формат виводу. З його допомогою розміри дисків та файлових систем відображаються в зручному вигляді з використанням одиниць виміру, таких як KB, MB, GB, замість байтів
+alias ls='lsd'        # Замінює стандартну команду ls на утиліту lsd. Це покращена версія ls з кольоровим виділенням, підтримкою іконок та сучасним виглядом
+
+
+# Аліаси для швидкої навігації
+alias cdd='cd ~/Documents'      # Для документів
+alias cddl='cd ~/Downloads'      # Для завантажень
+alias cdesk='cd ~/Desktop'       # Для робочого столу
+alias cmus='cd ~/Music'          # Для музики
+alias cpic='cd ~/Pictures'       # Для зображень
+alias cvid='cd ~/Videos'         # Для відео
+alias cconf='cd ~/.config'       # Для налаштувань
+
+alias .1='cd ..'            # Перехід на один рівень вище
+alias .2='cd ../..'         # Перехід на два рівні вище
+alias .3='cd ../../..'      # Перехід на три рівні вище
+alias .4='cd ../../../..'   # Перехід на чотири рівні вище
+
+# Управління системою
+alias c=clear                # Очистити термінал
+alias rb=reboot              # Перезавантаження комп'ютера
+alias sh='shutdown -h'       # Вимкнення ПК через вказаний час
+alias shn='shutdown now'     # Вимкнення ПК негайно
+alias shc='shutdown -c'      # Відмінити вимкнення пк
+
+# Файлова система
+alias mkdir='mkdir -pv'     # Створення директорії з виводом інформації та батьківськими папками
+alias rm='rm -I --preserve-root'  # Підтвердження при видаленні; захист від видалення кореневого каталогу.
+alias mv='mv -i'   # Підтвердження перед перезаписом файлів при переміщенні
+alias cp='cp -i'   # Підтвердження перед перезаписом файлів при копіюванні
+alias ln='ln -i'   # Підтвердження перед створенням посилань на файли
+
+
+# Утиліти
+alias bc='bc -l'       # Калькулятор з підтримкою дійсних чисел та математичних функцій
+alias bat='batcat'     # Утиліта BAT (Аналог утиліти CAT)
+alias mpip='curl ifconfig.me'     # Виводить на екран вашу публічну IP-адресу
+
+alias warpc="warp-cli connect"     # Підключення до Warp
+alias warpd="warp-cli disconnect"     # Відключення від Warp
+alias warps="warp-cli status"     # Статус з'єднання з Warp
+
+# Запуск скриптів
+alias sync_configs="$HOME/Files/Bash/sync_configs.sh"
+
+# ========== END ALIASES ==========
+
---
+++ linux/nvim/init.vim
@@ -0,0 +1,147 @@
+" === Мої налаштування ===
+
+"Включає номерацію строк
+set number 
+
+"Включає відображення номерів рядків у відносному форматі
+set relativenumber
+
+"Автовідступ
+set autoindent
+
+"Кількість пробілів при відступах
+set shiftwidth=4
+	
+"Кількість пробілів при табуляції
+set tabstop=4
+
+"Кількість пробілів замість символу табуляції
+set softtabstop=4
+
+"Включає підтримку миші
+set mouse=a
+
+
+" === Плагіни (vim-plug) ===
+
+call plug#begin()
+
+"Теми
+
+Plug 'crusoexia/vim-monokai'
+Plug 'folke/tokyonight.nvim'
+Plug 'sainnhe/sonokai'
+
+
+
+
+"Плагін "emmet"
+Plug 'mattn/emmet-vim'
+
+"Змінює вигляд нижньої полоси стану
+Plug 'vim-airline/vim-airline'
+
+"Відображає HEX кольори
+Plug 'ap/vim-css-color'
+
+"Каталог файлів у боковому меню
+Plug 'scrooloose/nerdtree'
+
+"Позволяє швидко закоментовувати текст
+Plug 'tpope/vim-commentary'
+
+"Іконки для VIM
+Plug 'ryanoasis/vim-devicons'
+
+"Мультикурсор
+Plug 'terryma/vim-multiple-cursors'
+
+" Копіювання в системний буфер
+Plug 'christoomey/vim-system-copy'
+
+call plug#end()
+" =======================================
+
+" === Горячі клавіші ===
+
+"NERDTree (Відкриває та закриває бокове меню файлів)
+nnoremap <C-b> :NERDTreeToggle<CR>
+
+
+"Включає можливість введення команд в укр розкладці
+map ' `
+map й q
+map ц w
+map у e
+map к r
+map е t
+map н y
+map г u
+map ш i
+map щ o
+map з p
+map х [
+map ї ]
+map ф a
+map і s
+map в d
+map а f
+map п g
+map р h
+map о j
+map л k
+map д l
+map ж ;
+map є '
+map я z
+map ч x
+map с c
+map м v
+map и b
+map т n
+map ь m
+map б ,
+map ю .
+map ʼ ~
+map Й Q
+map Ц W
+map У E
+map К R
+map Е T
+map Н Y
+map Г U
+map Ш I
+map Щ O
+map З P
+map Х {
+map Ї }
+map Ф A
+map І S
+map В D
+map А F
+map П G
+map Р H
+map О J
+map Л K
+map Д L
+map Ж :
+map Э "
+map Я Z
+map Ч X
+map С C
+map М V
+map И B
+map Т N
+map Ь M
+map Б <
+map Ю >
+" =======================================
+"
+" === Теми ===
+"colorscheme monokai
+colorscheme tokyonight
+let g:sonokai_style = 'andromeda'
+colorscheme sonokai
+
+
+
